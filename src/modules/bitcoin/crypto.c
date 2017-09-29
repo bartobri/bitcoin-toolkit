2fcb496aa667cc7000c4c96a6c43991c180dc3c5
Brian Barto <bartobrian@gmail.com>
2017-09-29 14:12:24 -0400

    Add crypto init function
    	modified:   src/modules/bitcoin/crypto.c

--- src/modules/bitcoin/crypto.c
+++ src/modules/bitcoin/crypto.c
@@ -1,8 +1,24 @@
 #include <gcrypt.h>
 
+static int isInit = 0;
+
+int crypto_init(void) {
+	if (!isInit) {
+		if (!gcry_check_version(GCRYPT_VERSION)) {
+			return 0;
+		}
+		isInit = 1;
+	}
+	return 1;
+}
+
 unsigned char *crypto_get_sha256 (unsigned char *s, size_t l) {
 	size_t i;
 	gcry_md_hd_t gc;
+	
+	if (!crypto_init()) {
+		// TODO - handle error
+	}
 	
 	// Initialize
 	gcry_md_open(&gc, GCRY_MD_SHA256, 0);
