fd4d5190f1ec05c802d58944317906f538f2576c
Brian Barto <bartobrian@gmail.com>
2017-10-02 18:02:52 -0400

    Delete old modules

--- src/modules_old/base58.h
+++
@@ -1,6 +0,0 @@
-#ifndef BASE58_H
-#define BASE58_H 1
-
-char *base58_encode(unsigned char *, size_t);
-
-#endif
--- src/modules_old/random.h
+++
@@ -1,6 +0,0 @@
-#ifndef RANDOM_H
-#define RANDOM_H 1
-
-unsigned char random_get_byte(void);
-
-#endif
