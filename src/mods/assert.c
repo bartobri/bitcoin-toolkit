c3371a7ee2f553c657b3181681de6256b4b5ccc7
Brian Barto <bartobrian@gmail.com>
2017-10-24 20:35:21 -0400

    Add assert and except mods to project to help with exception/error handling
    	modified:   Makefile
    	new file:   src/mods/assert.c
    	new file:   src/mods/assert.h
    	new file:   src/mods/except.c
    	new file:   src/mods/except.h

---
+++ src/mods/assert.c
@@ -0,0 +1,10 @@
+/* 
+ * $Id: H:/drh/idioms/book/RCS/except.doc,v 1.10 1997/02/21 19:43:55 drh Exp $
+ */
+#include "assert.h"
+
+const Except_T Assert_Failed = { "Assertion failed" };
+
+void (assert)(int e) {
+	assert(e);
+}
