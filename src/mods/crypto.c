55aee9982d078ddba8602d7cb858064d50a3d864
Brian Barto <bartobrian@gmail.com>
2017-10-27 18:11:42 -0400

    Add checks on input values and use gcry_md_write instead of gcry_md_putc
    	modified:   src/mods/crypto.c

--- src/mods/crypto.c
+++ src/mods/crypto.c
@@ -1,4 +1,5 @@
 #include <gcrypt.h>
+#include "assert.h"
 
 static int isInit = 0;
 
@@ -13,20 +14,17 @@ int crypto_init(void) {
 }
 
 unsigned char *crypto_get_sha256(unsigned char *s, size_t l) {
-	size_t i;
 	gcry_md_hd_t gc;
 	
-	if (!crypto_init()) {
-		// TODO - handle error
-	}
+	assert(s);
+	assert(l);
+	assert(crypto_init());
 	
 	// Initialize
 	gcry_md_open(&gc, GCRY_MD_SHA256, 0);
 	
 	// Crypt
-	for (i = 0; i < l; ++i) {
-		gcry_md_putc(gc, s[i]);
-	}
+	gcry_md_write(gc, s, l);
 	
 	// Return hash
 	// TODO - Don't return the digest directly. Copy it to another string
@@ -35,20 +33,17 @@ unsigned char *crypto_get_sha256(unsigned char *s, size_t l) {
 }
 
 unsigned char *crypto_get_rmd160(unsigned char *s, size_t l) {
-	size_t i;
 	gcry_md_hd_t gc;
 	
-	if (!crypto_init()) {
-		// TODO - handle error
-	}
+	assert(s);
+	assert(l);
+	assert(crypto_init());
 	
 	// Initialize
 	gcry_md_open(&gc, GCRY_MD_RMD160, 0);
 	
 	// Crypt
-	for (i = 0; i < l; ++i) {
-		gcry_md_putc(gc, s[i]);
-	}
+	gcry_md_write(gc, s, l);
 	
 	// Return hash
 	// TODO - Don't return the digest directly. Copy it to another string
