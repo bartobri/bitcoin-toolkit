ca3ebd556cc05d2a98ab53bce3f9bdcb3ba78914
Brian Barto <bartobrian@gmail.com>
2017-10-27 18:40:39 -0400

    Allocate a new uc string for the return value and close the gcrypt adt
    	modified:   src/mods/crypto.c

--- src/mods/crypto.c
+++ src/mods/crypto.c
@@ -1,5 +1,7 @@
 #include <gcrypt.h>
+#include <string.h>
 #include "assert.h"
+#include "mem.h"
 
 static int isInit = 0;
 
@@ -15,10 +17,13 @@ int crypto_init(void) {
 
 unsigned char *crypto_get_sha256(unsigned char *s, size_t l) {
 	gcry_md_hd_t gc;
+	unsigned char *r;
 	
 	assert(s);
 	assert(l);
 	assert(crypto_init());
+	
+	r = ALLOC(32);
 	
 	// Initialize
 	gcry_md_open(&gc, GCRY_MD_SHA256, 0);
@@ -26,18 +31,25 @@ unsigned char *crypto_get_sha256(unsigned char *s, size_t l) {
 	// Crypt
 	gcry_md_write(gc, s, l);
 	
+	// Copy hash
+	memcpy(r, gcry_md_read(gc, 0), 32);
+	
+	// Close gcrypt ADT
+	gcry_md_close(gc);
+	
 	// Return hash
-	// TODO - Don't return the digest directly. Copy it to another string
-	// and return it. Then free up the gcrypt handle.
-	return gcry_md_read(gc, 0);
+	return r;
 }
 
 unsigned char *crypto_get_rmd160(unsigned char *s, size_t l) {
 	gcry_md_hd_t gc;
+	unsigned char *r;
 	
 	assert(s);
 	assert(l);
 	assert(crypto_init());
+	
+	r = ALLOC(20);
 	
 	// Initialize
 	gcry_md_open(&gc, GCRY_MD_RMD160, 0);
@@ -45,8 +57,12 @@ unsigned char *crypto_get_rmd160(unsigned char *s, size_t l) {
 	// Crypt
 	gcry_md_write(gc, s, l);
 	
+	// Copy hash
+	memcpy(r, gcry_md_read(gc, 0), 20);
+	
+	// Close gcrypt ADT
+	gcry_md_close(gc);
+	
 	// Return hash
-	// TODO - Don't return the digest directly. Copy it to another string
-	// and return it. Then free up the gcrypt handle.
-	return gcry_md_read(gc, 0);
+	return r;
 }
