ccc369081cdd0f79be3648181b9fe84c9ecfd3b1
Brian Barto <bartobrian@gmail.com>
2017-10-27 15:10:09 -0400

    Add checks on the parameters and use memcpy instead of for loop.
    	modified:   src/mods/base58check.c

--- src/mods/base58check.c
+++ src/mods/base58check.c
@@ -1,39 +1,37 @@
 #include <stdlib.h>
+#include <string.h>
 #include "base58.h"
 #include "crypto.h"
+#include "mem.h"
+#include "assert.h"
 
 #define CHECKSUM_LENGTH 4
 
 char *base58check_encode(unsigned char *s, size_t l) {
-	size_t i;
 	unsigned char *scheck;
 	unsigned char *sha;
 	char *r;
 	
+	assert(s);
+	assert(l);
+	
 	// Get memory for the checksummed string
-	if ((scheck = malloc(l + CHECKSUM_LENGTH)) == NULL) {
-		// TODO: handle error
-	}
+	scheck = ALLOC(l + CHECKSUM_LENGTH);
 	
 	// Copy s to scheck
-	for (i = 0; i < l; ++i) {
-		scheck[i] = s[i];
-	}
+	memcpy(scheck, s, l);
 	
 	// SHA256(SHA256(s))
 	sha = crypto_get_sha256(crypto_get_sha256(s, l), 32);
 	
-	// First 4 bytes of checksum as last 4 bytes of return value
-	for (i = 0; i < CHECKSUM_LENGTH; ++i) {
-		scheck[l+i] = sha[i];
-	}
+	// First 4 bytes of checksum as last 4 bytes of scheck
+	memcpy(scheck + l, sha, CHECKSUM_LENGTH);
 	
 	// Encode checksumed string
 	r = base58_encode(scheck, l + CHECKSUM_LENGTH);
 	
 	// Free memory
 	free(scheck);
-	//free(sha);  // TODO - why can't i free this?
 	
 	return r;
 }
