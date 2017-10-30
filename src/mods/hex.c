647d950dc1226712f73eeba044a6e0e9b0094bd9
Brian Barto <bartobrian@gmail.com>
2017-10-30 19:00:33 -0400

    Make sure hex string is even numbers. Also use ALLOC for memory allocation.
    	modified:   src/mods/hex.c

--- src/mods/hex.c
+++ src/mods/hex.c
@@ -6,6 +6,8 @@
 #include "assert.h"
 #include "mem.h"
 
+#include <stdio.h>
+
 unsigned char hex_to_dec(char l, char r) {
 	unsigned char ret;
 
@@ -69,18 +71,17 @@ uint64_t hex_to_dec_substr(size_t offset, char *str, size_t len) {
 	return (uint64_t)r;
 }
 
-// TODO - validate string is fully hexidecimal and even numbered
 unsigned char *hex_str_to_uc(char *hex) {
 	size_t i, l;
 	unsigned char *raw;
 	
 	l = strlen(hex);
 	
-	if ((raw = malloc(l / 2)) == NULL) {
-		// TODO - handle memory error
-	}
+	assert(l % 2 == 0);
 	
-	for (i = 0; i < l; ++i, hex += 2) {
+	raw = ALLOC(l / 2);
+	
+	for (i = 0; i < l / 2; ++i, hex += 2) {
 		raw[i] = hex_to_dec(hex[0], hex[1]);
 	}
 	
