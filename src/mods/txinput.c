f6bea20933c4bb7ef9fd6267e4c87563939d1643
Brian Barto <bartobrian@gmail.com>
2017-11-06 19:06:38 -0500

    Add asserts to check input and replace the CHECK_ZERO marco
    	modified:   src/mods/txinput.c

--- src/mods/txinput.c
+++ src/mods/txinput.c
@@ -4,12 +4,14 @@
 #include "hex.h"
 #include "compactuint.h"
 #include "mem.h"
-
-#define CHECK_ZERO(x)      if (x == 0) return NULL;
+#include "assert.h"
 
 TXInput txinput_from_raw(unsigned char *raw, size_t l, size_t *c) {
 	size_t i, j;
 	TXInput r;
+
+	assert(raw);
+	assert(l);
 
 	NEW(r);
 	
@@ -17,35 +19,34 @@ TXInput txinput_from_raw(unsigned char *raw, size_t l, size_t *c) {
 	
 	// Transaction hash being spent
 	for (i = 0; i < 32; ++i, ++raw, --l, ++(*c)) {
-		CHECK_ZERO(l);
+		assert(l);
 		r->tx_hash[31-i] = *raw;
 	}
 	
 	// Output index of transcaction hash
 	for (i = 0; i < sizeof(r->index); ++i, ++raw, --l, ++(*c)) {
-		CHECK_ZERO(l);
+		assert(l);
 		r->index <<= 8;
 		r->index += *raw;
 	}
 	
 	// Unlocking Script Size
 	r->script_size = compactuint_get_value(raw, l, &j);
-	CHECK_ZERO(r->script_size);
 	raw += j;
 	*c += j;
 	l = (j > l) ? 0 : l - j;
-	CHECK_ZERO(l);
+	assert(l);
 	
 	// Unlocking Script
 	r->script_raw = ALLOC(r->script_size);
 	for (i = 0; i < r->script_size; ++i, ++raw, --l, ++(*c)) {
-		CHECK_ZERO(l);
+		assert(l);
 		r->script_raw[i] = *raw;
 	}
 	
 	// Sequence
 	for (i = 0; i < sizeof(r->sequence); ++i, ++raw, --l, ++(*c)) {
-		CHECK_ZERO(l);
+		assert(l);
 		r->sequence <<= 8;
 		r->sequence += *raw;
 	}
