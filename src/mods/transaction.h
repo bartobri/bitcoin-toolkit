06e649c61bbea9592e75c5545c03683e0fbbe60c
Brian Barto <bartobrian@gmail.com>
2017-11-06 19:23:26 -0500

    Make Trans a pointer to struct Trans
    	modified:   src/mods/transaction.h

--- src/mods/transaction.h
+++ src/mods/transaction.h
@@ -5,14 +5,15 @@
 #include "txinput.h"
 #include "txoutput.h"
 
-typedef struct {
+typedef struct Trans *Trans;
+struct Trans {
 	uint32_t  version;
 	uint64_t  input_count;
 	TXInput  *inputs;
 	uint64_t  output_count;
 	TXOutput *outputs;
 	uint32_t  lock_time;
-} *Trans;
+};
 
 Trans transaction_from_raw(unsigned char *, size_t);
 
