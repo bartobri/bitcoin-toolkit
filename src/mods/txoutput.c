3a0ed929be10305cb3e735be534cae8f4a70cbbb
Brian Barto <bartobrian@gmail.com>
2017-11-06 19:13:37 -0500

    Use assert for checking inputs
    	modified:   src/mods/txoutput.c

--- src/mods/txoutput.c
+++ src/mods/txoutput.c
@@ -3,39 +3,39 @@
 #include "txoutput.h"
 #include "hex.h"
 #include "compactuint.h"
+#include "mem.h"
+#include "assert.h"
 
-#define CHECK_ZERO(x)      if (x == 0) return NULL;
+//#define CHECK_ZERO(x)      if (x == 0) return NULL;
 
 TXOutput txoutput_from_raw(unsigned char *raw, size_t l, size_t *c) {
 	size_t i, j;
 	TXOutput r;
 	
-	if ((r = malloc(sizeof(*r))) == NULL) {
-		// TODO - handle memory error here
-	}
+	assert(raw);
+	assert(l);
+
+	NEW(r);
 	
 	*c = 0;
 	
 	// Output Amount
 	for (r->amount = 0, i = 0; i < sizeof(r->amount); ++i, ++raw, --l, ++(*c)) {
-		CHECK_ZERO(l);
+		assert(l);
 		r->amount += (((size_t)*raw) << (i * 8)); // Reverse byte order
 	}
 	
 	// Unlocking Script Size
 	r->script_size = compactuint_get_value(raw, l, &j);
-	CHECK_ZERO(r->script_size);
 	raw += j;
 	*c += j;
 	l = (j > l) ? 0 : l - j;
-	CHECK_ZERO(l);
+	assert(l);
 	
 	// Unlocking Script
-	if ((r->script_raw = malloc(r->script_size)) == NULL) {
-		// TODO - Handle memory error here
-	}
+	r->script_raw = ALLOC(r->script_size);
 	for (i = 0; i < r->script_size; ++i, ++raw, --l, ++(*c)) {
-		CHECK_ZERO(l);
+		assert(l);
 		r->script_raw[i] = *raw;
 	}
 	
