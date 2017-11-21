a972195571c3993b2f1d65a9834f97655ddbd539
Brian Barto <bartobrian@gmail.com>
2017-11-21 10:12:56 -0500

    Add assert checks
    	modified:   src/mods/serialize.c

--- src/mods/serialize.c
+++ src/mods/serialize.c
@@ -1,7 +1,11 @@
 #include <string.h>
 #include "serialize.h"
+#include "assert.h"
 
 unsigned char *serialize_uint32(unsigned char *dest, uint32_t src, int endian) {
+	
+	assert(dest);
+	assert(endian == SERIALIZE_ENDIAN_BIG || endian == SERIALIZE_ENDIAN_LIT);
 
 	if (endian == SERIALIZE_ENDIAN_LIT) {
 		*dest++ = (unsigned char)(src & 0x000000FF);
@@ -19,6 +23,10 @@ unsigned char *serialize_uint32(unsigned char *dest, uint32_t src, int endian) {
 }
 
 unsigned char *serialize_uchar(unsigned char *dest, unsigned char *src, int len) {
+	
+	assert(dest);
+	assert(src);
+	assert(len);
 	
 	memcpy(dest, src, len);
 	dest += len;
