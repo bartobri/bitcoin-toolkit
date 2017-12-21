879fb84644a8998d6a265fc2429f3b4519f8fa9d
Brian Barto <bartobrian@gmail.com>
2017-12-20 19:48:26 -0500

    Add flag to generate new private key in compressed WIP format. Also optional newline flag.
    	modified:   src/libs/btk_privkey.c

--- src/libs/btk_privkey.c
+++ src/libs/btk_privkey.c
@@ -9,15 +9,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
+#include "mods/privkey.h"
+
+/*
+ * Flags
+ */
+static int flag_source_new = 0;
+static int flag_output_newline = 0;
 
 int btk_privkey_init(int argc, char *argv[]) {
 	int o;
+	PrivKey key;
 	
 	// Check arguments
-	while ((o = getopt(argc, argv, "g")) != -1) {
+	while ((o = getopt(argc, argv, "nN")) != -1) {
 		switch (o) {
-			case 'g':
-				printf("Got g flag\n");
+			case 'n':
+				flag_source_new = 1;
+				break;
+			case 'N':
+				flag_output_newline = 1;
 				break;
 			case '?':
 				if (isprint(optopt))
@@ -26,6 +37,21 @@ int btk_privkey_init(int argc, char *argv[]) {
 					fprintf (stderr, "Unknown option character '\\x%x'.\n", optopt);
 				return EXIT_FAILURE;
 		}
+	}
+	
+	// Check for minimum flags
+	if (!flag_source_new) {
+		fprintf(stderr, "Missing source flag.\n");
+		return EXIT_FAILURE;
+	}
+	
+	// Execute flags
+	if (flag_source_new) {
+		key = privkey_new_compressed();
+		printf("%s", privkey_to_wif(key));
+
+		if (flag_output_newline)
+			printf("\n");
 	}
 	
 	return EXIT_SUCCESS;
