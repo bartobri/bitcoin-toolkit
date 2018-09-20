2ea78977f73825ce3562ee0806f32185ae485b15
Brian Barto <bartobrian@gmail.com>
2018-09-20 11:08:38 -0400

    Move all open/close curley brackets to their own line
    	modified:   src/mods/base32.c

--- src/mods/base32.c
+++ src/mods/base32.c
@@ -2,12 +2,12 @@
 #include <string.h>
 #include <stdint.h>
 #include "base32.h"
-#include "mem.h"
 #include "assert.h"
 
 static char *code_string = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
 
-void base32_encode(char *output, unsigned char *data, size_t data_len) {
+void base32_encode(char *output, unsigned char *data, size_t data_len)
+{
 	size_t i, output_len;
 
 	assert(output);
@@ -16,13 +16,15 @@ void base32_encode(char *output, unsigned char *data, size_t data_len) {
 
 	base32_encode_raw((unsigned char*)output, &output_len, data, data_len);
 
-	for (i = 0; i < output_len; ++i) {
+	for (i = 0; i < output_len; ++i)
+	{
 		output[i] = base32_get_char((uint8_t)output[i]);
 	}
 	output[i] = '\0';
 }
 
-void base32_encode_raw(unsigned char *output, size_t *output_len, unsigned char *data, size_t data_len) {
+void base32_encode_raw(unsigned char *output, size_t *output_len, unsigned char *data, size_t data_len)
+{
 	size_t i, j, k;
 	unsigned char *output_head = output;
 
@@ -31,11 +33,14 @@ void base32_encode_raw(unsigned char *output, size_t *output_len, unsigned char 
 	assert(data_len);
 
 	*output = 0;
-	for (i = 0, k = 0; i < data_len; ++i) {
-		for (j = 0; j < 8; ++j) {
+	for (i = 0, k = 0; i < data_len; ++i)
+	{
+		for (j = 0; j < 8; ++j)
+		{
 			*output <<= 1;
 			*output += ((data[i] << j) & 0x80) >> 7;
-			if ((++k % 5) == 0) {
+			if ((++k % 5) == 0)
+			{
 				output++;
 				*output = 0;
 			}
@@ -45,18 +50,22 @@ void base32_encode_raw(unsigned char *output, size_t *output_len, unsigned char 
 	output = output_head;
 }
 
-char base32_get_char(uint8_t c) {
+char base32_get_char(uint8_t c)
+{
 	assert(c < strlen(code_string));
 	return code_string[c];
 }
 
-int base32_get_raw(char c) {
+int base32_get_raw(char c)
+{
 	size_t i;
 
 	assert(c);
 
-	for (i = 0; i < strlen(code_string); ++i) {
-		if (c == code_string[i]) {
+	for (i = 0; i < strlen(code_string); ++i)
+	{
+		if (c == code_string[i])
+		{
 			return i;
 		}
 	}
