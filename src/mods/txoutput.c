065796ff5b1a69bc8983d453ff97e1ec98a4da48
Brian Barto <bartobrian@gmail.com>
2018-10-02 17:00:48 -0400

    Add error logging to tcoutput module
    	modified:   src/mods/txoutput.c

--- src/mods/txoutput.c
+++ src/mods/txoutput.c
@@ -3,6 +3,7 @@
 #include "txoutput.h"
 #include "hex.h"
 #include "compactuint.h"
+#include "error.h"
 #include "mem.h"
 #include "assert.h"
 
@@ -22,6 +23,7 @@ int txoutput_from_raw(TXOutput txoutput, unsigned char *input, size_t input_len)
 	{
 		if (input_len == 0)
 		{
+			error_log("Transaction output data is incomplete.");
 			return -1;
 		}
 		txoutput->amount += (((size_t)*input) << (i * 8)); // Reverse byte order
@@ -31,6 +33,7 @@ int txoutput_from_raw(TXOutput txoutput, unsigned char *input, size_t input_len)
 	r = compactuint_get_value(&txoutput->script_size, input, input_len);
 	if (r < 0)
 	{
+		error_log("Error while parsing compact size integer from transaction output data.");
 		return -1;
 	}
 	j = r; // quick fix - make prettier later
@@ -39,6 +42,7 @@ int txoutput_from_raw(TXOutput txoutput, unsigned char *input, size_t input_len)
 	input_len = (j > input_len) ? 0 : input_len - j;
 	if (input_len == 0)
 	{
+		error_log("Transaction output data is incomplete.");
 		return -1;
 	}
 	
@@ -48,6 +52,7 @@ int txoutput_from_raw(TXOutput txoutput, unsigned char *input, size_t input_len)
 	{
 		if (input_len == 0)
 		{
+			error_log("Transaction output data is incomplete.");
 			return -1;
 		}
 		txoutput->script_raw[i] = *input;
