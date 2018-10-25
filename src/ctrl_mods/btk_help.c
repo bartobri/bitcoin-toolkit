65b5a4a458e75f07345ce05d8810cf220ec492fe
Brian Barto <bartobrian@gmail.com>
2018-10-25 14:20:08 -0400

    Elaborating on the description and input options for the privkey command help info.
    	modified:   src/ctrl_mods/btk_help.c

--- src/ctrl_mods/btk_help.c
+++ src/ctrl_mods/btk_help.c
@@ -62,21 +62,45 @@ void btk_help_commands(void)
 
 void btk_help_privkey(void)
 {
-	printf("Usage: btk privkey [OPTION]\n");
+	printf("Usage: btk privkey [OPTIONS]\n");
 	printf("\n");
-	printf("Description here.\n");
+	printf("DESCRIPTION\n");
 	printf("\n");
-	printf("Input Options:\n");
-	printf("   -n   Generate a new private key from your local CSPRNG source.\n");
-	printf("   -w   Generate a private key from a WIF formatted string.\n");
-	printf("   -h   Generate a private key from a 64 byte hexidecimal string.\n");
-	printf("   -r   Generate a private key from a 32 byte raw binary string.\n");
-	printf("   -s   Generate a private key from an ASCII string processed through a SHA256 hashing algorithm.\n");
-	printf("   -d   Generate a private key from a decimal string.\n");
-	printf("   -b   Generate a private key from arbitrary binary data processed through a SHA256 hashing algorithm.\n");
+	printf("   The privkey command can create and manipulate private keys in a variety of\n");
+	printf("   fun and interesting ways. It supports compressed and uncompressed keys,\n");
+	printf("   mainnet and testnet, and all sorts of input and output formats. See each\n");
+	printf("   option description for more info.\n");
+	printf("\n");
+	printf("   By convention, input options are all lowercase and output options are all\n");
+	printf("   uppercase.\n");
+	printf("\n");
+	printf("INPUT OPTIONS\n");
+	printf("\n");
+	printf("   Note that all btk input options are designed such that input should be provided\n");
+	printf("   via pipe or redirection. none of these options take input as an argument on the\n");
+	printf("   command line. In the absence of data via pipe or redirection, btk\n");
+	printf("   may prompt the user for input if the input option allows for ASCII-only data\n");
+	printf("   (as opposed to raw binary, for example).\n");
+	printf("\n");
+	printf("   -n         new       Use 32 bytes of random data from your local CSPRNG as the input source.\n");
+	printf("   -w         wif       Input is a WIF formatted string.\n");
+	printf("   -h         hex       Input is a 64 character hexidecimal string.\n");
+	printf("   -r         raw       Input is 32 bytes of raw binary data.\n");
+	printf("   -s         string    Input is an ASCII string that should be processed through a SHA256 hashing algorithm to generate a 32 byte private key.\n");
+	printf("   -d         decimal   Input is a decimal string.\n");
+	printf("   -b         blob      Input is arbitrary binary data that should be processed through a SHA256 hashing algorithm to generate a 32 byte private key.\n");
+	printf("\n");
+	printf("   If no input option is spectified, btk will attemmpt to guess the input option, based on input contents, in the following order:\n");
+	printf("\n");
+	printf("   1. If the input is all numerical characters, btk assumes the -d option.\n");
+	printf("   2. If the input is exactly 64 hexidecimal characters, btk assumes the -h option.\n");
+	printf("   3. If the input is a properly encoded wif string with a valid checksum, btk assumes the -w option.\n");
+	printf("   4. If the input is all ASCII characters, btk assumes the -s option.\n");
+	printf("   5. If the input is exactly 32 bytes long characters, btk assumes the -r option.\n");
+	printf("   6. If all the above checks fail, btk will display an error message requiring you to specify an input option.\n");
 	printf("\n");
 	printf("Output Format Options:\n");
-	printf("   -W   Print private key as a WIF formmated string.\n");
+	printf("   -W   Print private key as a WIF formmated string. (default)\n");
 	printf("   -H   Print private key as a 64-65 byte hexidecimal string.\n");
 	printf("   -R   Print private key as 32 bytes of raw binary data.\n");
 	printf("   -D   Print private key as a (very large) decimal.\n");
@@ -87,8 +111,6 @@ void btk_help_privkey(void)
 	printf("\n");
 	printf("Output Misc Options:\n");
 	printf("   -N   Do NOT print a newline character.\n");
-	printf("\n");
-	printf("Network Options:\n");
 	printf("   -T   Format output for TESTNET\n");
 	printf("\n");
 }
